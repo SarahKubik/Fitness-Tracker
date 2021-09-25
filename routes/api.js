6fd1c51da0835074fa12162e66bbcdc39c741e68
Sarah <81164320+SarahKubik@users.noreply.github.com>
2021-09-25 17:33:08 -0400

    created api routes

---
+++ routes/api.js
@@ -0,0 +1,31 @@
+const router = require ("express").Router();
+const workout = require ("../models/workout.js");
+
+router.get("/api/workouts", (req, res) =>{
+    workout.find()
+    .then ( results=> res.json(results))
+    .catch (err=> res.json(err))
+})
+
+router.post("/api/workouts", (req, res)=>{
+    workout.create({})
+    .then (results=> res.json(results))
+    .catch (err=> res.json(err))
+})
+
+router.put("/api/workouts/:id", (req,res)=>{
+    workout.findbyIdandUpdate(
+        req.params.id,
+        {$push : {exercises: req.body}}
+    )
+    .then( results=> res.json(results))
+    .catch(err=> res.json(err))
+})
+
+router.get("/api/workouts/range", (req, res) =>{
+    workout.find()
+    .then(results=> res.json(results))
+    .catch(err=> res.json (err))
+})
+
+module.exports = router;
