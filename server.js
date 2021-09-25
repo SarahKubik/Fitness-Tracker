cf0939c7bc9852d76f0d2981a963944ff00610db
Sarah <81164320+SarahKubik@users.noreply.github.com>
2021-09-25 16:15:07 -0400

    create server connection to db

--- server.js
+++ server.js
@@ -1,2 +1,26 @@
-const { Mongoose } = require("mongoose")
+// Dependency to connect to server
+const express = require("express");
+const mongoose = require("mongoose");
+
+
+// Basic express server and port setup
+const app = express();
+const PORT = 27017;
+
+// Connecting to the database
+const url = 'mongodb://127.0.0.1:27017/workout';
+
+(async () => {
+    try {
+        await mongoose.connect(url, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false,
+            useCreateIndex: true
+        });
+        console.log(`MongoDB Connected: ${url}`);
+    } catch (err) {
+        console.error(err);
+    }
+})();
 
