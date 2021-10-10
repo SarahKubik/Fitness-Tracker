579430a02da3239f71874af8e2d8411991924164
skk <skkdetroit@gmail.com>
2021-10-10 17:23:47 -0400

    update database connection

--- server.js
+++ server.js
@@ -25,19 +25,41 @@ app.use(express.static("public"));
 // start mongodb in terminal
 // db
 
-const PORT = 3000;
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
-mongoose.connect(URI, {
-  useUnifiedTopology: true,
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+// const PORT = 3000;
+// const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
+// mongoose.connect(URI, {
+//   useUnifiedTopology: true,
+//   useNewUrlParser: true,
+//   useFindAndModify: false
+// });
 
-// requires the content in the models folder
-app.use(require("./routes/api.js"));
+// // requires the content in the models folder
+// app.use(require("./routes/api.js"));
 
-//  Listens for server port
-app.listen(PORT, () => {
-    console.log(`App running on port ${PORT}!`);
-  });
+// //  Listens for server port
+// app.listen(PORT, () => {
+//     console.log(`App running on port ${PORT}!`);
+//   });
 
+  const uri = `mongodb+srv://${process.env.DB_HOST}`;
+  const options = {
+    user: process.env.DB_USER,
+    pass: process.env.DB_PASS,
+    dbName: process.env.DB_NAME,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+  };
+  mongoose.connect(uri, options).then(
+    _ => {
+      console.info('Database connection stablished');
+      app.listen(port, function() {
+        debug(`listening on port ${port}`);
+      });
+    },
+    error => {
+      console.error('Database connection failed:', error);
+      throw new Error('Could not connect to the database');
+    }
+  );
