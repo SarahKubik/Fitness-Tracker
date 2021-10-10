1456ebd4a4eab00ae23ea0592701b96675199160
skk <skkdetroit@gmail.com>
2021-10-10 18:22:35 -0400

    update server and MONGO URI

--- server.js
+++ server.js
@@ -6,12 +6,14 @@ const mongoose = require("mongoose");
 
 
 // Basic express server and port setup
-const PORT = 27017;
+const PORT = 3001;
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
 const app = express();
 
 
 // requires the content in the models folder
 const db = require("./models");
+const Workout = require("./models/workout");
 
 app.use(logger("dev"));
 
@@ -25,46 +27,43 @@ app.use(express.static("public"));
 // start mongodb in terminal
 // db
 
-// const PORT = 3000;
-// const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
-// mongoose.connect(URI, {
-//   useUnifiedTopology: true,
-//   useNewUrlParser: true,
-//   useFindAndModify: false
-// });
+mongoose.connect( MONGODB_URI, {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
+});
 
-// // requires the content in the models folder
-// app.use(require("./routes/api.js"));
+// routes
+app.use(require("./routes/api.js"));
 
-// //  Listens for server port
-// app.listen(PORT, () => {
-//     console.log(`App running on port ${PORT}!`);
-//   });
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
 
-  const uri = `mongodb+srv://${process.env.DB_HOST}`;
-  const options = {
-    user: process.env.DB_USER,
-    pass: process.env.DB_PASS,
-    dbName: process.env.DB_NAME,
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true
-  };
-  mongoose.connect(uri, options).then(
-    _ => {
-      console.info('Database connection stablished');
-      app.listen(port, function() {
-        debug(`listening on port ${port}`);
-      });
-    },
-    error => {
-      console.error('Database connection failed:', error);
-      throw new Error('Could not connect to the database');
-    },
 
-    new Promise((resolve, reject) => {
-      reject('error');
-    }).catch((error) => {})
+  //   newPromise((resolve, reject) => {
+  //   reject('error');
+  // }).catch((error) => {})
 
-  );
+});
+
+  // const url = `mongodb+srv://${process.env.DB_HOST}`;
+  // const options = {
+  //   user: process.env.DB_USER,
+  //   pass: process.env.DB_PASS,
+  //   dbName: process.env.DB_NAME,
+  //   useNewUrlParser: true,
+  //   useUnifiedTopology: true,
+  //   useFindAndModify: false,
+  //   useCreateIndex: true
+  // };
+  // mongoose.connect(uri, options).then(
+  //   _ => {
+  //     console.info('Database connection stablished');
+  //     app.listen(port, function() {
+  //       debug(`listening on port ${port}`);
+  //     });
+  //   },
+  //   error => {
+  //     console.error('Database connection failed:', error);
+  //     throw new Error('Could not connect to the database');
+  //   },
