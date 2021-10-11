c58a5d44d5b5c1f3aa1f889d0eef0742f5eea283
skk <skkdetroit@gmail.com>
2021-10-11 12:26:46 -0400

    update file paths

--- routes/app.js
+++ routes/app.js
@@ -3,16 +3,16 @@ const db = require("../models");
 const path = require("path");
 
 router.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname + "/../public/index.html"));
+    res.sendFile(path.join(__dirname + "/public/index.html"));
 });
 
 
-router.get("/exercise", (req, res) => {
-    res.sendFile(path.join(__dirname + "/../public/exercise.html"));
+router.get("/exercise", (req, res) =>
+    res.sendFile(path.join(`${__dirname} + "/public/exercise.html"));
 });
 
 router.get("/stats", (req, res) => {
-    res.sendFile(path.join(__dirname + "/../public/stats.html"));
+    res.sendFile(path.join(__dirname + "/public/stats.html"));
 });
 
 router.get("/api/workouts", (req, res) => {
